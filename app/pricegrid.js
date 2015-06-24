1b58db4e8506c0ade0111347ea3c8b6772f4469a
Evan Wang <evan.wang22@gmail.com>
2015-06-24 07:03:16 -0700

    Reformat whitespace

--- app/pricegrid.js
+++ app/pricegrid.js
@@ -1,11 +1,24 @@
+var API_BASE_URL = 'http://localhost:3000'
+
 var getCurrentTabURL = function getCurrentTabURL(){
-	return chrome.tabs.getCurrent(function(tab){
-		return tab.url;
-	})
+  return chrome.tabs.getCurrent(function(tab){
+    return tab.url;
+  });
 };
 
 var checkIfAmazonProductURL = function checkIfAmazonProductURL(url){
-	var amazonRegExp = /.*amazon.com\/[a-z]{2}\/product\//;
-	return amazonRegExp.test(url)
-}
+  var amazonRegExp = /.*amazon.com\/[a-z]{2}\/product\//;
+  return amazonRegExp.test(url);
+};
 
+var findProductID = function findProductID(url){
+  var urlAfterProduct = url.split('/product/')[1];
+  return urlAfterProduct.split('/')[0];
+};
+
+var getProductDetails = function getProductDetails(productID){
+  var detailsPromise = $.ajax({
+    url: API_BASE_URL + '/search',
+    data: { product_id: productID }
+  });
+};
--- spec/pricegrid.spec.js
+++ spec/pricegrid.spec.js
@@ -1,33 +1,62 @@
 describe('pricegrid', function() {
-	describe('#getCurrentTabURL', function() {
-		beforeEach(function() {
-			var fakeTab = {'url':"www.test.com"};
-			
-			var chromeSpy = spyOn(window, 'chrome');
-			var tabsSpy = jasmine.createSpyObj('tabs',['getCurrent']);
-			
-			tabsSpy.getCurrent.and.callFake(function(callback){
-				return callback(fakeTab)
-			});
-			chromeSpy.tabs = tabsSpy;
-		});
+  describe('#getCurrentTabURL', function() {
+    beforeEach(function() {
+      var fakeTab = {'url':"www.test.com"};
 
-		it('gets the URL of the current tab', function() {
-			expect(getCurrentTabURL()).toEqual('www.test.com');
-		});
-	});
+      var chromeSpy = spyOn(window, 'chrome');
+      var tabsSpy = jasmine.createSpyObj('tabs',['getCurrent']);
 
-	describe('#checkIfAmazonProductURL', function(){
-		it('returns true if the URL is for an amazon product', function() {
-			var amazonProductURL = "http://www.amazon.com/gp/product/7382HA";
-			expect(checkIfAmazonProductURL(amazonProductURL)).toBe(true);
-		});
+      tabsSpy.getCurrent.and.callFake(function(callback){
+        return callback(fakeTab)
+      });
+      chromeSpy.tabs = tabsSpy;
+    });
 
-		it('returns false if the URL is not for an amazon product', function(){
-			var someOtherURL = "http://google.com";
-			var amazonHomepageURL = "http://amazon.com";
-			expect(checkIfAmazonProductURL(someOtherURL)).toBe(false);
-			expect(checkIfAmazonProductURL(amazonHomepageURL)).toBe(false);
-		});
-	});
+    it('gets the URL of the current tab', function() {
+      expect(getCurrentTabURL()).toEqual('www.test.com');
+    });
+  });
+
+  describe('#checkIfAmazonProductURL', function(){
+    it('returns true if the URL is for an amazon product', function() {
+      var amazonProductURL = "http://www.amazon.com/gp/product/7382HA";
+      expect(checkIfAmazonProductURL(amazonProductURL)).toBe(true);
+    });
+
+    it('returns false if the URL is not for an amazon product', function(){
+      var someOtherURL = "http://google.com";
+      var amazonHomepageURL = "http://amazon.com";
+      expect(checkIfAmazonProductURL(someOtherURL)).toBe(false);
+      expect(checkIfAmazonProductURL(amazonHomepageURL)).toBe(false);
+    });
+  });
+
+  describe('#findProductID', function() {
+    var amazonProductURL = 'http://www.amazon.com/gp/product/7382HA'
+    it('returns the ID of the product based on the URL', function(){
+      expect(findProductID(amazonProductURL)).toEqual('7382HA');
+    });
+  });
+
+  describe('#getProductDetails', function() {
+
+    beforeEach(function(){
+      var fakeResponseData = {};
+      this.jqueryAjaxSpy = spyOn($, 'ajax');
+
+      this.jqueryAjaxSpy.and.returnValue(fakeResponseData);
+    });
+
+    it('it calls a backend service', function(){
+      getProductDetails('123FUN456');
+      expect(this.jqueryAjaxSpy).toHaveBeenCalledWith({
+        url: API_BASE_URL + "/search",
+        data: { product_id: '123FUN456' }
+      })
+    });
+
+    it('returns details for the given product ID', function(){
+
+    })
+  });
 });
